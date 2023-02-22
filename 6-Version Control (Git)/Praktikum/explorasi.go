da6b19d522500c56c52baa6a3295c23424ab1831
habil <habiedgar111@gmail.com>
2023-02-22 14:38:03 +0700

    print 1-7

---
+++ 6-Version Control (Git)/Praktikum/explorasi.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	//melakukan print 1-7
+	fmt.Println("print 1")
+	fmt.Println("print 2")
+	fmt.Println("print 3")
+	fmt.Println("print 4")
+	fmt.Println("print 5")
+	fmt.Println("print 6")
+	fmt.Println("print 7")
+}
