5e669943da9ab9182f7e8f8dbdf4c81475976edb
habil <habiedgar111@gmail.com>
2023-02-24 20:48:14 +0700

    menambahkan prioritas1-1

---
+++ 8-Data Structure/Praktikum/Eksplorasi1-1/eksplorasi1-1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var bilangan int
+	fmt.Println("masukan bilangan")
+	fmt.Scanln(&bilangan)
+	fmt.Println(primeNumber(bilangan))
+}
+
+func primeNumber(bil int) string {
+
+	//big O sqrt(n)
+	if bil <= 1 {
+		return "Bukan Bilangan Prima"
+	}
+	for i := 2; i < int(math.Sqrt(float64(bil))); i++ {
+		if bil%i == 0 {
+			return "Bukan Bilangan Prima"
+		}
+	}
+
+	return "Bilangan Prima"
+}
