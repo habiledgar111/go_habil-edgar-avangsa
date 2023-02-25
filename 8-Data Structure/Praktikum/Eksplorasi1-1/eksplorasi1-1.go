0224ddaef29c78e813cb5f3115a0e150b119d585
habil <habiedgar111@gmail.com>
2023-02-25 14:53:35 +0700

    menyelesaikan secsion 8

--- 8-Data Structure/Praktikum/Eksplorasi1-1/eksplorasi1-1.go
+++ 8-Data Structure/Praktikum/Eksplorasi1-1/eksplorasi1-1.go
@@ -10,9 +10,34 @@ func main() {
 		{4, 5, 6},
 		{9, 8, 9},
 	}))
+
+	//intputan tambahan
+	fmt.Println(countMatrix([][]int{
+		{1, 2, 3, 4, 5},
+		{6, 7, 8, 9, 10},
+		{11, 12, 13, 14, 15},
+		{16, 17, 18, 19, 20},
+		{21, 22, 23, 24, 25},
+	}))
 }
 
 func countMatrix(matrix [][]int) int {
-	fmt.Println(matrix)
-	return 0
+	sum1 := 0
+	sum2 := 0
+	for i := 0; i < len(matrix); i++ {
+		for j := 0; j < len(matrix); j++ {
+			if i == j {
+				sum1 += matrix[i][j]
+			}
+			if i+j == len(matrix)-1 {
+				sum2 += matrix[i][j]
+			}
+		}
+	}
+
+	if sum1 > sum2 {
+		return sum1 - sum2
+	}
+
+	return sum2 - sum1
 }
