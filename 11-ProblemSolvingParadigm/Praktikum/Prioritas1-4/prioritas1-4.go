c3b7f27deea2a9a08f6bf1e856f76de6316c31d6
habil <habiedgar111@gmail.com>
2023-03-05 18:06:43 +0700

    selesai prioritas 2

---
+++ 11-ProblemSolvingParadigm/Praktikum/Prioritas1-4/prioritas1-4.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func simpleEquations(a, b, c int) interface{} {
+	//formula 1 x+y+z
+	//formula 2 xyz
+	//formula 3 x*x + y*y + z*z
+	for x := 1; x <= b; x++ {
+		for y := 1; y <= b; y++ {
+			for z := 1; z <= b; z++ {
+				if x+y+z == a && x*y*z == b && (x*x)+(y*y)+(z*z) == c {
+					return []int{x, y, z}
+				}
+			}
+		}
+	}
+	return "no solution"
+}
+func main() {
+	fmt.Println(simpleEquations(1, 2, 3))
+	fmt.Println(simpleEquations(6, 6, 14))
+	fmt.Println(simpleEquations(12, 12, 28))
+	fmt.Println(simpleEquations(7, 12, 17))
+}
