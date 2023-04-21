03bdb57c01f9acb0fc833f2c855ff57eaeaa051c
habil <habiedgar111@gmail.com>
2023-04-21 10:03:39 +0700

    selesai mengerjakan

---
+++ 024_clean_architecture/praktikum/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"sec024/praktikum/config"
+	"sec024/praktikum/usecase"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	config.Connection()
+
+	e := echo.New()
+	e.GET("/users", usecase.GetAll)
+	e.POST("/users")
+}
