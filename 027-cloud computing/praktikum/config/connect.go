a21d861e0bb6df6e975de8e49eeb657ab112ff51
habil <habiedgar111@gmail.com>
2023-06-09 17:11:51 +0700

    selesai cloud computing

---
+++ 027-cloud computing/praktikum/config/connect.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func Open() error {
+	dbname := ""
+	dsn := "root:Mbahbambang123@tcp(localhost:3306)/codecompetence?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	DB = db
+	if err != nil {
+		return err
+	}
+	return nil
+}
